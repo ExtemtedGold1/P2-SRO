d4486bf74b5892f7e4c57fd695fc3fad5f31a7e5
ExtemtedGold1 <wiktorbielecki31@gmail.com>
2024-01-15 21:40:21 +0100

    Revert "small changes"
    This reverts commit eb0834f7

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home/home';
 import { Register } from './components/register/register';
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from "./config/firebase";
 import {Login} from "./components/Login/login";
 function App() {
   return (
